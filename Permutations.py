306f2467f898d4348c5622ba48a158ffca50edfd
Mashookh Khan <mashooq.ahmed175@gmail.com>
2024-12-20 23:55:38 +0530

    12/20/24 commit

---
+++ Permutations.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        result = []
+
+        def helper(path, remaining):
+            if not remaining:
+                result.append(path)
+                return
+            
+            for i in range(len(remaining)):
+                helper(path+[remaining[i]],remaining[:i]+remaining[i+1:])
+        
+        helper([],nums)
+        return result
